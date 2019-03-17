d6b61e32734bfce055a6b5b67ac65f55511e3d0d
David <djtover94@gmail.com>
2019-03-17 15:47:19 +0200

    Started on Tree

---
+++ homework/Tree.cpp
@@ -0,0 +1,29 @@
+#include "Tree.hpp"
+
+bool Tree::contains(int value){
+
+}
+bool Tree::insert(int value){
+
+}
+int Tree::left(int value){
+
+}
+int Tree::parent(int value){
+
+}
+void Tree::print(){
+
+}
+bool Tree::remove(int value){
+
+}
+int Tree::right(int value){
+
+}
+Node* Tree::root(){
+    return Tree::start
+}
+int Tree::size(){
+    return Tree::size
+}
--- homework/Tree.hpp
+++ homework/Tree.hpp
@@ -1,1 +1,43 @@
-ode
+#include <iostream>
+using namespace std;
+
+
+
+class Tree {
+
+    typedef struct Node{
+    int key;
+    Node *right;
+    Node *left;
+}Node;
+
+Node *start;
+int amount;   
+
+Node* createNode(int value)
+{
+    Node *newNode = new Node();
+
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+public:
+    Tree(Node *first){
+        Tree::start= first
+        size=1;
+        
+    }
+    bool insert(int value);
+    bool remove(int value);
+    int size();
+    bool contains(int value);
+    Node* root();
+    int parent(int value);
+    int left(int value);
+    int right(int value);
+    void print();
+
+};
