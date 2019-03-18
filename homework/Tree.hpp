147c99aa50bf2a007a6b6759f857fe88bb34102a
David <djtover94@gmail.com>
2019-03-18 17:59:04 +0200

    Added comments

--- homework/Tree.hpp
+++ homework/Tree.hpp
@@ -11,60 +11,36 @@ class Tree
         Node *right;
         Node *left;
     };
+
+
+
     // The fields for the BST
     Node *start;
     int amount;
-    // A private method to create a Node
-    Node *createNode(int value)
-    {
-        Node *newNode = new Node();
 
-        newNode->key = value;
-        newNode->left = NULL;
-        newNode->right = NULL;
-        return newNode;
-    }
-    void addPrivate(int value, Node *p)
-    {
-        if (start == NULL)
-        {
-            start = createNode(value);
-        }
-        else if (value < p->key)
-        {
-            if (p->left != NULL)
-            {
-                addPrivate(value, p->left);
-            }
-            else
-            {
-                p->left = createNode(value);
-            }
-        }
-        else if (value > p->key)
-        {
-            if (p->right != NULL)
-            {
-                addPrivate(value, p->right);
-            }
-            else
-            {
-                p->right = createNode(value);
-            }
-        }
-        else
-        {
-            cout << "That value has been entered already" << endl;
-        }
-    }
+
+
+    // Declaration of private methods
+    Node *createNode(int value);
+    void addPrivate(int value, Node *p);
+    
+
+
+
     // Start of public methods
+ 
   public:
+
+  
     //Constructor
     Tree()
     {
         Tree::start = NULL;
         Tree::amount = 0;
     }
+
+
+
     // Declaration for all the methods
     void insert(int value);
     void remove(int value);
